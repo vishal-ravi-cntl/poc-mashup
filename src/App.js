d3c718e46a2ca161eccb4a75d9b14536a625529d
Vishal Ravi <vishal_ravi@condenast.com>
2024-04-25 15:44:58 +0530

    Extracted Data ( month - sales )from table object

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import "./App.css";
+import BarChart from "./Components/BarChart";
 import Kpi from "./Components/Kpi";
 import { SessionProvider } from "./Context/QlikContext";
 
 function App() {
   const salesAppId = "799ab440-b740-4c54-b941-bb1413ce6696";
   const salesKpiObjectID = "DhusPup";
+  const salesTableObjectID = "wRXTg";
   const subscriptionAppId = "63f2c786-ae10-447a-916c-bf9286e58876";
   const subscriptionKpiObjectID = "DhusPup";
+  const subscriptiontableObjectID = "wRXTg";
 
   return (
     <div className="App">
       <SessionProvider appName={salesAppId}>
         <Kpi objectId={salesKpiObjectID} />
+        <BarChart objectId={salesTableObjectID} />
       </SessionProvider>
       <SessionProvider appName={subscriptionAppId}>
         <Kpi objectId={subscriptionKpiObjectID} />
+        <BarChart objectId={salesTableObjectID} />
       </SessionProvider>
     </div>
   );
---
+++ src/Components/BarChart.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { useGetObject } from "../Hooks/qlik-hooks/Doc";
+import {
+  useGetLayout,
+  useGetHyperCubeData,
+} from "../Hooks/qlik-hooks/GenericObject";
+import { useSession } from "../Context/QlikContext";
+
+function BarChart({ objectId }) {
+  const app = useSession();
+  const obj = useGetObject(app, { params: [objectId] });
+  const objLayout = useGetLayout(obj, { params: [], invalidations: true });
+  const hyperCubeData = useGetHyperCubeData(obj);
+
+  const [data, setData] = useState([]);
+  const [showChart, setShowChart] = useState(false);
+  const [totalRows, setTotalRows] = useState(-1);
+  const rowsPerPage = 3000;
+
+  //   console.log("object Layout:", objLayout);
+
+  useEffect(() => {
+    if (objLayout.qResponse != null) {
+      const temptotalRows = objLayout.qResponse.qHyperCube.qSize.qcy;
+      setTotalRows(temptotalRows);
+      setData([]);
+      for (let i = 0; i < totalRows; i += rowsPerPage) {
+        hyperCubeData.call("/qHyperCubeDef", [
+          {
+            qTop: i,
+            qLeft: 1,
+            qWidth: 1,
+            qHeight: Math.min(rowsPerPage, totalRows - i),
+          },
+          {
+            qTop: i,
+            qLeft: 2,
+            qWidth: 1,
+            qHeight: Math.min(rowsPerPage, totalRows - i),
+          },
+        ]);
+      }
+    }
+  }, [objLayout, totalRows]);
+
+  useEffect(() => {
+    if (hyperCubeData.qResponse != null) {
+      // console.log("hypercube", hyperCubeData);
+
+      let tableData =
+        hyperCubeData?.qResponse[0].qMatrix?.map((item, index) => {
+          let obj = {};
+          obj.month = hyperCubeData?.qResponse[0].qMatrix[index][0].qText;
+          obj.sales = hyperCubeData?.qResponse[1].qMatrix[index][0].qText;
+          return obj;
+        }) || [];
+
+      console.log({ tableData });
+
+      setData((data) => [...data, ...tableData]);
+      setShowChart(true);
+    }
+  }, [hyperCubeData]);
+  return (
+    <div>
+      {showChart ? (
+        <div>{"Data Value: " + data[0].sales}</div>
+      ) : (
+        <div>Bar chart data Loading..</div>
+      )}
+    </div>
+  );
+}
+
+export default BarChart;
