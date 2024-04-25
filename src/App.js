66d436e5a6c3304ada81b9b6a0931eabbcff9b7f
Vishal Ravi <vishal_ravi@condenast.com>
2024-04-25 14:42:01 +0530

    Session Created using qlik-hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import QlikContext from "./Hooks/qlik-hooks/connect/QlikProvider";
 
 function App() {
-  return <div className="App">Yo!</div>;
+  const salesAppId = "799ab440-b740-4c54-b941-bb1413ce6696";
+  const subscriptionAppId = "63f2c786-ae10-447a-916c-bf9286e58876";
+  const salesConfig = {
+    host: "dash.condenast.com",
+    isSecure: true,
+    appname: salesAppId,
+  };
+
+  return (
+    <div className="App">
+      <QlikContext config={salesConfig}>Yo!</QlikContext>
+    </div>
+  );
 }
 
 export default App;
